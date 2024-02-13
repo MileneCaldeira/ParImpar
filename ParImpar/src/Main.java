a7a12d21b64ebf459e99ec0f4c078d18a2505afd
Milene <mihdsc2004@gmail.com>
2024-02-12 21:06:55 -0300

    Commit

---
+++ ParImpar/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner (System.in);
+	System.out.println("Informe um número: ");
+ double N = sc.nextDouble();
+	if (N%2 == 0) {
+		
+		System.out.println("PAR");
+		
+	}else {
+		System.out.println("IMPAR");
+	}
+	}
+
+}
